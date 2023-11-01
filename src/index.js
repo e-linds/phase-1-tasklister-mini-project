c4710a04b3b31923891ec223704c2e9d01aa7ae1
Emmi Lindsey <emmilindsey@gmail.com>
2023-11-01 15:29:22 -0600

    complete tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+document.getElementById("create-task-form").addEventListener("submit", (e) => {
+  e.preventDefault()
+  const todo = e.target.new_task_description.value
+
+  const li = document.createElement("li")
+  li.textContent = todo
+  document.querySelector("#tasks").append(li)
+
+  const btn = document.createElement("button")
+  li.append(btn)
+  btn.textContent = "all done!"
+
+  btn.addEventListener("click", () => {
+    li.remove()
+  })
+
+  document.getElementById("create-task-form").reset()
+
+})
+
 });
